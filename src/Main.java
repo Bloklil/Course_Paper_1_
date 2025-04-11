30e2d57961140beb220dd37bbfb14cf057f6bbfd
Anton S <hitman.soldirs@gmail.com>
2025-04-11 09:41:35 +0800

    Part 1. last difficulty level. correction 0

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,45 @@ public class Main {
     public static Employee[] employees = new Employee[10];
 
     public static void main(String[] args) {
-        initializeEmployees();
+        EmployeeBook employeeBook = new EmployeeBook(10);
+        employeeBook.addEmployee(new Employee("Зигмунд Фрейд Фурункулович", 1, 129211));
+        employeeBook.addEmployee(new Employee("Левитан Агрипина Фёдоровна", 2, 111111));
+        employeeBook.addEmployee(new Employee("Мёд Григорий Медведкович", 4, 99000));
+        employeeBook.addEmployee(new Employee("Шестопал Фёдор Борисович", 5, 110900));
+        employeeBook.addEmployee(new Employee("Сметанин Максим Валентинович", 1, 111698));
+        employeeBook.addEmployee(new Employee("Черномол Дарья Валерьевна", 2, 111451));
+        employeeBook.addEmployee(new Employee("Вольфрам Зульфия Амурфовна", 3, 112345));
+        employeeBook.addEmployee(new Employee("Черепан Максим Леонидович", 4, 113890));
+        employeeBook.addEmployee(new Employee("Прудников Артём Сергеевич", 5, 119999));
 
-        Employee.printAllEmployees(employees);
-        Employee.printFuuNames(employees);
-        Employee.printAllEmployeesInDepartment(employees, 3);  //затраты по отделу
-        Employee.indexSalary(employees, 10);  //индексация на 10%
-        Employee.printEmployeesDep(employees, 1);
-        Employee.indexSalaryDep(employees, 100, 1); //индексация отдела
-        Employee.printThresholdEmployessSalaryMin(employees, 111000);
-        Employee.printThresholdEmployessSalaryMax(employees, 129000);
-
-        int totalSalary = Employee.calculatedTotalSalary(employees);
-        System.out.println("Общая зарплата: " + totalSalary + " рублей.");
-        var min = Employee.findEmployeeWithMinSalary(employees);
-        System.out.println("Сотрудник с минимально ЗП: " + min);
-        var max = Employee.findEmployeeWithMaxSalary(employees);
-        System.out.println("Сотрудник с максимальной ЗП: " + max);
-        int average = Employee.averageSalary(employees);
-        System.out.println("Средняя зарплата " + average + " рублей.");
-        var minDep = Employee.employeeWithMinSalaryByDepartment(employees, 3); //min по департаменту
-        System.out.println(minDep);
-        var maxDep = Employee.employeeWithMaxSalaryByDepartment(employees, 5); //max по департаменту
-        System.out.println(maxDep);
-        double averageDep = Employee.averageSalaryByDepartment(employees, 6);  //средняя по департаменту
-        System.out.println("Средняя ЗП по отделу: " + averageDep + " рублей.");
-
-    }
-
-    public static void initializeEmployees() {
-        employees[0] = new Employee("Зигмунд Фрейд Фурункулович", 1, 111_129);
-        employees[1] = new Employee("Левитан Агрипина Фёдоровна", 2, 89_890);
-        employees[2] = new Employee("Мёд Григорий Медведкович", 3, 78_111);
-        employees[3] = new Employee("Шестопал Фёдор Борисович", 1, 111_900);
-        employees[4] = new Employee("Сметанин Максим Валентинович", 4, 111_456);
-        employees[5] = new Employee("Черномол Дарья Валерьевна", 5, 101_001);
-        employees[6] = new Employee("Вольфрам Зульфия Амурфовна", 4, 123_456);
-        employees[7] = new Employee("Черепан Максим Леонидович", 3, 101_099);
-        employees[8] = new Employee("Прудников Артём Сергеевич", 5, 99_909);
+        employeeBook.printAllEmployees();
+        final int totalSalary = employeeBook.calculatedTotalSalary();
+        System.out.println("Сумма зарплат всех сотрудников: " + totalSalary + " рублей.");
+        final Employee employeeWithMinSalary = employeeBook.findEmployeeWithMinSalary();
+        System.out.println("Сотрудник с минимальной зарплатой: \n" + employeeWithMinSalary);
+        final Employee employeeWithMaxSalary = employeeBook.findEmployeeWithMaxSalary();
+        System.out.println("Сотрудник с максимальной зарплатой: \n" + employeeWithMaxSalary);
+        final int i = employeeBook.averageSalary();
+        System.out.println("Средняя зарплата по отделам: \n" + i + " рублей.");
+        employeeBook.printAllFuuNames();
+        employeeBook.printAllEmployeesInDepartment(1);
+        employeeBook.indexSalary(111); //индексация всех.
+        byte minDepartament = 1;
+        var minDerp = employeeBook.employeeWithMinSalaryByDepartment(1);
+        System.out.println("Сотрудник в отделе " + minDepartament + " с минимальной зарплатой: \n" + minDerp);
+        byte maxDepartament = 2;
+        var maxDerp = employeeBook.employeeWithMaxSalaryByDepartment(2);
+        System.out.println("Сотрудник в отделе " + maxDepartament + " с максимальной зарплатой: \n" + maxDerp);
+        byte averageDepartament = 1;
+        var averageDep = employeeBook.averageSalaryByDepartment(1);
+        System.out.println("Средняя зарплата по отделу " + averageDepartament + ":\n" + averageDep + " рублей.");
+        employeeBook.printEmployeesDep(1);
+        employeeBook.indexSalaryDep(10, 1); //индексация в департаменте
+        employeeBook.printThresholdEmployessSalaryMin(230000); //порог ниже ЗП
+        employeeBook.printThresholdEmployessSalaryMax(290000); // порог выше ЗП
+        employeeBook.removeEmployee(1); // удаление по id
+        employeeBook.printAllEmployees();
+        Employee employeeById = employeeBook.getEmployeeById(2);
+        System.out.println("Работник по ID: \n" + employeeById);
     }
 }
